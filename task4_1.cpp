74922d36cde800bdccfa54e3570571c0382c1584
shysa <sanir93@gmail.com>
2020-05-06 01:19:25 +0300

    trying 4 task yet

--- task4_1.cpp
+++ task4_1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <functional>
+#include <vector>
 
 // ----------------- AVL TREE ---------------------------------------------------------------
 template <typename T, typename comparator = std::less<T>>
@@ -87,6 +88,24 @@ private:
         }
     }
 
+    Node* addInternal(Node *node, const T& data, int& position) {
+        if (!node) {
+            return new Node(data);
+        }
+
+        node->nodes++;
+
+        if (cmp(data, node->data)) {
+            // итоговая позиция точно не меньше чем nodes(right) + 1
+            position += getNodes(node->right) + 1;
+            node->left = addInternal(node->left, data, position);
+        } else {
+            node->right = addInternal(node->right, data, position);
+        }
+
+        return doBalance(node);
+    }
+
     /*Node* findAndRemoveMin(Node *node) {
         while (node->left) {
             node = node->left;
@@ -116,29 +135,96 @@ private:
         return doBalance(node);
     }
 
-    Node* addInternal(Node *node, const T& data, int& position) {
-        if (!node) {
-            return new Node(data);
-        }
-
-        node->nodes++;
-
-        if (cmp(node->data, data) || node->data == data) {
-            position += getNodes(node->right) + 1;
-            node->right = addInternal(node->right, data, position);
-        } else {
-            node->left = addInternal(node->left, data, position);
-        }
-
-        return doBalance(node);
-    }
-
-    Node* removeInternal(Node *node, const T& data, int& position) {
+    Node* removeInternal(Node *node, const int& position) {
         if (!node) {
             return nullptr;
         }
 
+        if (position >= node->nodes) {
+            return node;
+        }
 
+        int current = 0;
+        std::stack<Node *> nodes;
+
+        while (true) {
+            int nodes_right = getNodes(node->right);
+
+            if (position - current > nodes_right) {
+                nodes.push(node);
+                node = node->left;
+                current += nodes_right + 1;
+            }
+            else if (position - current < nodes_right) {
+                if (node->right) {
+                    nodes.push(node);
+                    node = node->right;
+                } else {
+                    break;
+                }
+            } else {
+                // нашли нужную позицию для удаления
+                Node *left = node->left;
+                Node *right = node->right;
+                int key = node->data;
+
+                delete node;
+
+                // если у удаляемого элемента не было правого и левого потомков,
+                // то просто поправим предыдущий узел
+                if (!right) {
+                    if (!left) {
+                        // берем предыдущий пройденный узел
+                        if (!nodes.empty()) {
+                            node = nodes.top();
+                            nodes.pop();
+
+                            // чистим связи на удаленный узел
+                            if (node->data > key) {
+                                node->left = nullptr;
+                            } else {
+                                node->right = nullptr;
+                            }
+                            // и корректируем показатель nodes
+                            node->nodes--;
+                        } else {
+                            return nullptr;
+                        }
+                    } else {
+                        // если был левый потомок, встаем на него
+                        node = left;
+                    }
+                } else {
+                    // если правый потомок существует, то заменяем на минимальный из правого поддерева
+                    Node *min = findMin(right);
+                    min->right = removeMin(right);
+                    min->left = left;
+
+                    fixNodes(min);
+                    return doBalance(min);
+                }
+                break;
+            }
+        }
+
+        // исправляем nodes у всех пройденных узлов
+        while (!nodes.empty()) {
+            Node *tmp = nodes.top();
+            node->nodes--;
+
+            if (tmp->data > node->data) {
+                tmp->left = node;
+            } else {
+                tmp->right = node;
+            }
+
+            node = doBalance(node);
+            nodes.pop();
+        }
+
+        return node;
+
+        /*
 
         if (cmp(node->data, data)) {
             node->right = removeInternal(node->right, data);
@@ -162,7 +248,7 @@ private:
 
             return doBalance(min);
         }
-        return doBalance(node);
+        return doBalance(node);*/
     }
 
     void destroyTree(Node *node) {
@@ -183,7 +269,7 @@ public:
 
     bool Has(const T& data);
     void Add(const T& data, int& position);
-    void Remove(const T& data, int& position);
+    void Remove(const int& position);
 };
 
 // ----------------------------------------------------------------------------
@@ -208,8 +294,8 @@ void AVLTree<T, comparator>::Add(const T &data, int& position) {
 }
 
 template<typename T, typename comparator>
-void AVLTree<T, comparator>::Remove(const T &data, int& position) {
-    root = removeInternal(root, data, position);
+void AVLTree<T, comparator>::Remove(const int& position) {
+    root = removeInternal(root, position);
 }
 
 
@@ -217,10 +303,12 @@ void AVLTree<T, comparator>::Remove(const T &data, int& position) {
 
 int main() {
     AVLTree<int> avlTree;
+    std::vector<int> result;
 
     int command;
     int value;
     int n = 0;
+    int position;
 
     std::cin >> n;
 
@@ -228,16 +316,23 @@ int main() {
         std::cin >> command >> value;
         switch (command) {
             case 1: {
-                avlTree.Add(value);
+                position = 0;
+                avlTree.Add(value, position);
+                result.push_back(position);
                 break;
             }
             case 2: {
-                avlTree.Remove(value);
+                position = value;
+                avlTree.Remove(position);
                 break;
             }
             default:
                 break;
         }
+    }
+
+    for (int item : result) {
+        std::cout << item << std::endl;
     }
 
     return 0;
