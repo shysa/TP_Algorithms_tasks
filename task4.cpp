dadd5732695329b784891db0fb6f4e0f7fe393c2
shysa <sanir93@gmail.com>
2020-05-28 21:54:14 +0300

    task4 for 3x3

--- task4.cpp
+++ task4.cpp
@@ -1,86 +1,118 @@
 #include <iostream>
-#include <vector>
 #include <cassert>
-#include <set>
+#include <array>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
 
-// ------------------------------- LIST --------------------------------------------
-class ListGraph {
+// ------------------------------- GAME FIELD --------------------------------------------
+const char SIZE = 9;
+const std::array<char, SIZE> FINAL = {1, 2, 3, 4, 5, 6, 7, 8, 0};
+
+struct GameStateHash;
+
+class GameState {
 private:
-    struct vertexData {
-        // текущий кратчайший путь
-        // для всех +inf, кроме 0й вершины
-        size_t currentMinPath;
+    std::array<char, SIZE> field;
+    char emptyPos;
 
-        // в начале каждой вершине ставим отсутствие родительской вершины = -1
-        int parent;
+    bool CanMoveLeft() const;
+    bool CanMoveRight() const;
+    bool CanMoveDown() const;
+    bool CanMoveUp() const;
 
-        vertexData() : currentMinPath(-1), parent(-1) {}
-    };
+    GameState MoveLeft() const;
+    GameState MoveRight() const;
+    GameState MoveDown() const;
+    GameState MoveUp() const;
 
-    // смежная вершина + вес ребра
-    std::vector<std::vector<std::pair<int, int>>> adjacencyLists;
+    bool InversionParity(const std::array<char, SIZE>& array);
+    friend std::ostream& operator<<(std::ostream &os, const GameState &state);
+    friend struct GameStateHash;
+    friend std::string GetSolution(const std::array<char, SIZE> &_field);
 public:
-    ListGraph(int size): adjacencyLists(size) {};
-
-    ~ListGraph() = default;
-
-    void AddEdge(int from, int to, int weight) {
-        assert(0 <= from && from < adjacencyLists.size());
-        assert(0 <= to && to < adjacencyLists.size());
-        assert(weight >= 0);
-
-        adjacencyLists[from].push_back(std::make_pair(weight, to));
-        adjacencyLists[to].push_back(std::make_pair(weight, from));
-    }
-
-    std::vector<std::pair<int, int>> GetNextVertices(int vertex) const {
-        assert(0 <= vertex && vertex < adjacencyLists.size());
-        return adjacencyLists[vertex];
-    }
-
-    // -------------------------------- TASK ----------------------------------------
-    int findMinPath(int from, int to) {
-        std::vector<vertexData> states(adjacencyLists.size());
-
-        // pair(d[i], i)
-        std::set<std::pair<int, int>> queue;
-
-        queue.insert({0, from});
-        states[from].currentMinPath = 0;
-        states[from].parent = from;
-
-        while (!queue.empty()) {
-            std::pair<int, int> current = *queue.begin();
-            queue.erase(queue.begin());
-
-            // pair(weight, i)
-            std::vector<std::pair<int, int>> next = GetNextVertices(current.second);
-            for (const auto& vertex : next) {
-                if (states[current.second].currentMinPath + vertex.first < states[vertex.second].currentMinPath) {
-                    // релаксируем ребро в set
-                    queue.erase({states[vertex.second].currentMinPath, vertex.first});
-
-                    queue.insert(vertex);
-                    states[vertex.second].currentMinPath = states[current.second].currentMinPath + vertex.first;
-                    states[vertex.second].parent = current.first;
-
-                }
+    GameState(const std::array<char, SIZE> &array): field(array) {
+        emptyPos = -1;
+        for (int i = 0; i < SIZE; i++) {
+            if (field[i] == 0) {
+                emptyPos = i;
+                break;
             }
         }
+        assert(emptyPos != -1);
+    }
+    GameState(const GameState &_source): field(_source.field), emptyPos(_source.emptyPos) {};
 
-        return states[to].currentMinPath;
+    bool IsComplete() const {
+        return field == FINAL;
+    }
+
+    bool IsSolvable() {
+        return InversionParity(field);
+    }
+
+    bool operator==(const GameState &other) const {
+        return other.field == field;
     }
 };
+// ------------------------------------------------------------------------------------
+bool GameState::CanMoveLeft() const {
+    return emptyPos % 3 != 2;
+}
+bool GameState::CanMoveRight() const {
+    return emptyPos % 3 != 0;
+}
+bool GameState::CanMoveDown() const {
+    return emptyPos > 2;
+}
+bool GameState::CanMoveUp() const {
+    return emptyPos < 6;
+}
+// ------------------------------------------------------------------------------------
+GameState GameState::MoveLeft() const {
+    assert(CanMoveLeft());
 
+    GameState newState(*this);
+    std::swap(newState.field[emptyPos], newState.field[emptyPos + 1]);
+    newState.emptyPos++;
 
+    return newState;
+}
+GameState GameState::MoveRight() const {
+    assert(CanMoveRight());
 
+    GameState newState(*this);
+    std::swap(newState.field[emptyPos], newState.field[emptyPos - 1]);
+    newState.emptyPos--;
+
+    return newState;
+}
+GameState GameState::MoveDown() const {
+    assert(CanMoveDown());
+
+    GameState newState(*this);
+    std::swap(newState.field[emptyPos], newState.field[emptyPos - 3]);
+    newState.emptyPos -= 3;
+
+    return newState;
+}
+GameState GameState::MoveUp() const {
+    assert(CanMoveUp());
+
+    GameState newState(*this);
+    std::swap(newState.field[emptyPos], newState.field[emptyPos + 3]);
+    newState.emptyPos += 3;
+
+    return newState;
+}
+// ------------------------------------------------------------------------------------
 // чтобы имелось решение, нужно четное количество инверсий
-bool InversionParity(int** array, int size) {
+bool GameState::InversionParity(const std::array<char, SIZE> &array) {
     int countWrongPairs = 0;
 
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (array[i] < array[j]) {
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = i + 1; j < SIZE; j++) {
+            if (array[i] && array[j] && array[i] < array[j]) {
                 countWrongPairs++;
             }
         }
@@ -88,23 +120,117 @@ bool InversionParity(int** array, int size) {
 
     return countWrongPairs % 2 == 0;
 }
+// ------------------------------------------------------------------------------------
+std::ostream& operator<<(std::ostream &os, const GameState &state) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            os << static_cast<int>(state.field[i * 3 + j]) << " ";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+// ------------------------------------------------------------------------------------
+struct GameStateHash {
+    size_t operator()(const GameState &state) const {
+        return std::hash<std::string>{}(state.field.data());
+    }
+};
+// ------------------------------------------------------------------------------------
+std::string GetSolution(const std::array<char, SIZE> &_field) {
+    GameState startState(_field);
 
-int main() {
-    int N = 0;      // количество городов
-    int M = 0;      // количество дорог
-    int from;
-    int to;
-    int weight;
-
-    std::cin >> N >> M;
-    ListGraph listGraph(N);
-    for (int i = 0; i < M; i++) {
-        std::cin >> from >> to >> weight;
-        listGraph.AddEdge(from, to, weight);
+    if (!startState.IsSolvable()) {
+        return "-1";
     }
 
-    std::cin >> from >> to;
-    std::cout << listGraph.findMinPath(from, to);
+    std::queue<GameState> queue;
+    // {field, L/R/D/U} - как попали в field
+    std::unordered_map<GameState, char, GameStateHash> way;
+
+    way[startState] = 'S';      // S = start
+    queue.push(startState);
+
+    while (true) {
+        GameState currentState = queue.front();
+        queue.pop();
+
+        if (currentState.IsComplete()) {
+            break;
+        }
+
+        if (currentState.CanMoveLeft()) {
+            GameState nextState = currentState.MoveLeft();
+            if (way.find(nextState) == way.end()) {
+                way[nextState] = 'L';
+                queue.push(nextState);
+            }
+        }
+        if (currentState.CanMoveRight()) {
+            GameState nextState = currentState.MoveRight();
+            if (way.find(nextState) == way.end()) {
+                way[nextState] = 'R';
+                queue.push(nextState);
+            }
+        }
+        if (currentState.CanMoveDown()) {
+            GameState nextState = currentState.MoveDown();
+            if (way.find(nextState) == way.end()) {
+                way[nextState] = 'D';
+                queue.push(nextState);
+            }
+        }
+        if (currentState.CanMoveUp()) {
+            GameState nextState = currentState.MoveUp();
+            if (way.find(nextState) == way.end()) {
+                way[nextState] = 'U';
+                queue.push(nextState);
+            }
+        }
+    }
+
+    std::string path;
+    GameState state(FINAL);
+
+    std::cout << state << std::endl;
+
+    while (way[state] != 'S') {
+        switch (way[state]) {
+            case 'L': {
+                path += 'L';
+                state = state.MoveRight();
+                break;
+            }
+            case 'R': {
+                path += 'R';
+                state = state.MoveLeft();
+                break;
+            }
+            case 'D': {
+                path += 'D';
+                state = state.MoveUp();
+                break;
+            }
+            case 'U': {
+                path += 'U';
+                state = state.MoveDown();
+                break;
+            }
+        }
+        std::cout << state << std::endl;
+    }
+
+    std::reverse(path.begin(), path.end());
+    return path;
+}
+// ------------------------------------------------------------------------------------
+
+
+
+int main() {
+    std::array<char, SIZE> field = {2, 3, 1, 4, 5, 6, 7, 8, 0};
+
+    std::cout << GetSolution(field) << std::endl;
 
     return 0;
 }
