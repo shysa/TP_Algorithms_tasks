21186b89644312b07d20eead749cd738ddd6e1aa
shysa <sanir93@gmail.com>
2020-05-28 20:23:09 +0300

    task 3 complete

--- task3.cpp
+++ task3.cpp
@@ -1,126 +1,94 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-#include <queue>
-
-struct IGraph {
-    virtual ~IGraph() {}
-    // Добавление ребра от from к to.
-    virtual void AddEdge(int from, int to) = 0;
-
-    virtual int VerticesCount() const  = 0;
-
-    virtual std::vector<int> GetNextVertices(int vertex) const = 0;
-    virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
-};
+#include <set>
 
 // ------------------------------- LIST --------------------------------------------
-class ListGraph: public IGraph {
+class ListGraph {
 private:
     struct vertexData {
-        // текущее кратчайшее расстояние до вершины
+        // текущий кратчайший путь
         // для всех +inf, кроме 0й вершины
         size_t currentMinPath;
 
         // в начале каждой вершине ставим отсутствие родительской вершины = -1
         int parent;
 
-        // подсчет количества кратчайших путей
-        // для всех 0, кроме 0й вершины
-        int paths;
-
-        vertexData() : currentMinPath(-1), parent(-1), paths(0) {}
+        vertexData() : currentMinPath(-1), parent(-1) {}
     };
 
-    std::vector<std::vector<int>> adjacencyLists;
+    // смежная вершина + вес ребра
+    std::vector<std::vector<std::pair<int, int>>> adjacencyLists;
 public:
     ListGraph(int size): adjacencyLists(size) {};
-    ListGraph(const IGraph& graph): adjacencyLists(graph.VerticesCount()) {
-        for (int i = 0; i < graph.VerticesCount(); i++) {
-            adjacencyLists[i] = graph.GetNextVertices(i);
-        }
-    }
-    ~ListGraph() override = default;
 
-    void AddEdge(int from, int to) override  {
+    ~ListGraph() = default;
+
+    void AddEdge(int from, int to, int weight) {
         assert(0 <= from && from < adjacencyLists.size());
         assert(0 <= to && to < adjacencyLists.size());
+        assert(weight >= 0);
 
-        adjacencyLists[from].push_back(to);
-        adjacencyLists[to].push_back(from);
+        adjacencyLists[from].push_back(std::make_pair(weight, to));
+        adjacencyLists[to].push_back(std::make_pair(weight, from));
     }
 
-    int VerticesCount() const override {
-        return (int)adjacencyLists.size();
-    }
-
-    std::vector<int> GetNextVertices(int vertex) const override {
+    std::vector<std::pair<int, int>> GetNextVertices(int vertex) const {
         assert(0 <= vertex && vertex < adjacencyLists.size());
         return adjacencyLists[vertex];
     }
 
-    std::vector<int> GetPrevVertices(int vertex) const override {
-        assert(0 <= vertex && vertex < adjacencyLists.size());
-        std::vector<int> prevVertices;
-
-        for (int from = 0; from < adjacencyLists.size(); from++) {
-            for (int to : adjacencyLists[from]) {
-                if (to == vertex) {
-                    prevVertices.push_back(from);
-                }
-            }
-        }
-
-        return prevVertices;
-    }
-
     // -------------------------------- TASK ----------------------------------------
-    int coutMinPaths(int from, int to) {
+    int findMinPath(int from, int to) {
         std::vector<vertexData> states(adjacencyLists.size());
-        std::queue<int> queue;
 
-        queue.push(from);
+        // pair(d[i], i)
+        std::set<std::pair<int, int>> queue;
+
+        queue.insert({0, from});
         states[from].currentMinPath = 0;
-        states[from].paths = 1;
+        states[from].parent = from;
 
         while (!queue.empty()) {
-            int current = queue.front();
-            queue.pop();
+            std::pair<int, int> current = *queue.begin();
+            queue.erase(queue.begin());
 
-            std::vector<int> next = GetNextVertices(current);
-            for (int vertex : next) {
-                // из текущей вершины можем попасть в следующую дешевле, чем по уже имеющемуся пути?
-                if (states[current].currentMinPath + 1 < states[vertex].currentMinPath) {
-                    queue.push(vertex);
-                    states[vertex].currentMinPath = states[current].currentMinPath + 1;
-                    states[vertex].parent = current;
-                    states[vertex].paths = states[current].paths;
-                } else if (states[current].currentMinPath + 1 == states[vertex].currentMinPath) {
-                    states[vertex].paths += states[current].paths;
+            // pair(weight, i)
+            std::vector<std::pair<int, int>> next = GetNextVertices(current.second);
+            for (const auto& vertex : next) {
+                if (states[current.second].currentMinPath + vertex.first < states[vertex.second].currentMinPath) {
+                    // релаксируем ребро в set
+                    queue.erase({states[vertex.second].currentMinPath, vertex.first});
+
+                    queue.insert(vertex);
+                    states[vertex.second].currentMinPath = states[current.second].currentMinPath + vertex.first;
+                    states[vertex.second].parent = current.first;
+
                 }
             }
         }
 
-        return states[to].paths;
+        return states[to].currentMinPath;
     }
 };
 
 
 int main() {
-    int v = 0;      // Кол-во вершин
-    int e = 0;      // Кол-во ребер
+    int N = 0;      // количество городов
+    int M = 0;      // количество дорог
     int from;
     int to;
+    int weight;
 
-    std::cin >> v >> e;
-    ListGraph listGraph(v);
-    for (int i = 0; i < e; i++) {
-        std::cin >> from >> to;
-        listGraph.AddEdge(from, to);
+    std::cin >> N >> M;
+    ListGraph listGraph(N);
+    for (int i = 0; i < M; i++) {
+        std::cin >> from >> to >> weight;
+        listGraph.AddEdge(from, to, weight);
     }
 
     std::cin >> from >> to;
-    std::cout << listGraph.coutMinPaths(from, to);
+    std::cout << listGraph.findMinPath(from, to);
 
     return 0;
 }
---
+++ task4.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <set>
+
+// ------------------------------- LIST --------------------------------------------
+class ListGraph {
+private:
+    struct vertexData {
+        // текущий кратчайший путь
+        // для всех +inf, кроме 0й вершины
+        size_t currentMinPath;
+
+        // в начале каждой вершине ставим отсутствие родительской вершины = -1
+        int parent;
+
+        vertexData() : currentMinPath(-1), parent(-1) {}
+    };
+
+    // смежная вершина + вес ребра
+    std::vector<std::vector<std::pair<int, int>>> adjacencyLists;
+public:
+    ListGraph(int size): adjacencyLists(size) {};
+
+    ~ListGraph() = default;
+
+    void AddEdge(int from, int to, int weight) {
+        assert(0 <= from && from < adjacencyLists.size());
+        assert(0 <= to && to < adjacencyLists.size());
+        assert(weight >= 0);
+
+        adjacencyLists[from].push_back(std::make_pair(weight, to));
+        adjacencyLists[to].push_back(std::make_pair(weight, from));
+    }
+
+    std::vector<std::pair<int, int>> GetNextVertices(int vertex) const {
+        assert(0 <= vertex && vertex < adjacencyLists.size());
+        return adjacencyLists[vertex];
+    }
+
+    // -------------------------------- TASK ----------------------------------------
+    int findMinPath(int from, int to) {
+        std::vector<vertexData> states(adjacencyLists.size());
+
+        // pair(d[i], i)
+        std::set<std::pair<int, int>> queue;
+
+        queue.insert({0, from});
+        states[from].currentMinPath = 0;
+        states[from].parent = from;
+
+        while (!queue.empty()) {
+            std::pair<int, int> current = *queue.begin();
+            queue.erase(queue.begin());
+
+            // pair(weight, i)
+            std::vector<std::pair<int, int>> next = GetNextVertices(current.second);
+            for (const auto& vertex : next) {
+                if (states[current.second].currentMinPath + vertex.first < states[vertex.second].currentMinPath) {
+                    // релаксируем ребро в set
+                    queue.erase({states[vertex.second].currentMinPath, vertex.first});
+
+                    queue.insert(vertex);
+                    states[vertex.second].currentMinPath = states[current.second].currentMinPath + vertex.first;
+                    states[vertex.second].parent = current.first;
+
+                }
+            }
+        }
+
+        return states[to].currentMinPath;
+    }
+};
+
+
+
+// чтобы имелось решение, нужно четное количество инверсий
+bool InversionParity(int** array, int size) {
+    int countWrongPairs = 0;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (array[i] < array[j]) {
+                countWrongPairs++;
+            }
+        }
+    }
+
+    return countWrongPairs % 2 == 0;
+}
+
+int main() {
+    int N = 0;      // количество городов
+    int M = 0;      // количество дорог
+    int from;
+    int to;
+    int weight;
+
+    std::cin >> N >> M;
+    ListGraph listGraph(N);
+    for (int i = 0; i < M; i++) {
+        std::cin >> from >> to >> weight;
+        listGraph.AddEdge(from, to, weight);
+    }
+
+    std::cin >> from >> to;
+    std::cout << listGraph.findMinPath(from, to);
+
+    return 0;
+}
